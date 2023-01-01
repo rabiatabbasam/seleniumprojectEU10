2c5b62baf05bf3213c804cff7050e2debb737bf3
Rabia Tabassum <rabiatabbasam@yahoo.com>
2023-01-01 13:13:00 +0000

    day 5: TestNG_Intro and TestNG Selenium

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -1,13 +1,63 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 public class TestNG_Intro {
 
-    @Test
+@BeforeClass
+   public void setupMethod(){
+    System.out.println("--> BeforeClass is running!");
+}
+  @AfterClass
+  public void tearDownClass(){
+      System.out.println("---> AfterClass is running!");
+  }
+
+  @BeforeMethod
+   public void setup(){
+       System.out.println("--> BeforeMethod is running!");
+   }
+
+   @AfterMethod
+   public void tearDownMethod(){
+       System.out.println("--> AfterMethod is running!");
+   }
+
+    @Test(priority = 1)
     public void test1(){
         System.out.println("Test 1 is running...");
+
+        //ASSERT EQUALS: compare 2 of the same thing
+
+        String actual = "apple";
+        String expected = "apple2";
+
+        Assert.assertEquals(actual,expected);
+
     }
+
+    @Test (priority = 2)
+    public void test2(){
+        System.out.println("Test 2 is running...");
+
+
+        //AssertTrue
+        String actual = "apple";
+        String expected = "apple";
+
+        Assert.assertTrue(actual.equals(expected),"your message will go here");
+
+
+
+
+}
+
+
+
+
+
+
 
 
 
