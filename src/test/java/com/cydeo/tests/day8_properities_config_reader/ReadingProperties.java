5d9bf2e232c1ede4f87e9b58f0c387ec892fd516
Rabia Tabassum <rabiatabbasam@yahoo.com>
2023-01-10 11:42:30 +0000

    day 8: reading Properties

---
+++ src/test/java/com/cydeo/tests/day8_properities_config_reader/ReadingProperties.java
@@ -0,0 +1,29 @@
+package com.cydeo.tests.day8_properities_config_reader;
+
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadingProperties {
+
+    @Test
+    public void reading_from_properties_test() throws IOException {
+        //1- Create the object of properties
+
+        Properties properties = new Properties();
+
+        //2- We need to open the file in java memory
+        FileInputStream file = new FileInputStream("configuration.properties");
+
+        //3- Load the properties using fileInputStream object
+        properties.load(file);
+
+        //4- Use "properties2 object to read value
+        System.out.println("properties.getProperty(\"browser\") = " + properties.getProperty("browser"));
+
+
+    }
+
+}
