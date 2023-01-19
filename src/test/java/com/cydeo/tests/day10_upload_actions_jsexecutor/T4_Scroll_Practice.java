ad1a588a5328b15e0772fd4111beaa8f16bbb201
Rabia Tabassum <rabiatabbasam@yahoo.com>
2023-01-19 10:51:41 +0000

    day 10:Upload_Practice.java

---
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T4_Scroll_Practice.java
@@ -0,0 +1,34 @@
+package com.cydeo.tests.day10_upload_actions_jsexecutor;
+
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class T4_Scroll_Practice {
+
+    @Test
+    public void scroll_test(){
+        //1- Open a Chrome Browser
+        //2- Go to: https://practice.cydeo.com/
+        Driver.getDriver().get("https://practice.cydeo.com/");
+
+     //Locate the element
+        WebElement poweredByCydeo = Driver.getDriver().findElement(By.xpath("//div[contains(text(),\"Powered by\")]"));
+
+        //3- Scroll down to "Powered by Cydeo"
+        //4- Scroll using Actions class "moveTo(element)"method
+        Actions actions = new Actions(Driver.getDriver());
+
+        BrowserUtils.sleep(2);
+        actions.moveToElement(poweredByCydeo).perform();
+
+        //Assert.assertTrue(poweredByCydeo.isDisplayed());
+
+
+    }
+
+
+}
---
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T6_JavaScriptExecutor.java
@@ -0,0 +1,50 @@
+package com.cydeo.tests.day10_upload_actions_jsexecutor;
+
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.Test;
+
+public class T6_JavaScriptExecutor {
+
+    @Test
+    public void javascript_executor_test1(){
+        //TC#6: Scroll using JavaScriptExecutor
+        //1- Open A Chrome browser
+        //2- Go to: https://practice.cydeo.com/infinite_scroll
+        Driver.getDriver().get("https://practice.cydeo.com/infinite_scroll");
+
+
+       // ((JavascriptExecutor)Driver.getDriver()).executeScript("");
+        //3- Use below javaScript method and scroll
+
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        //a. 750 pixels down 10 times.
+        for (int i = 0; i < 10 ; i++) {
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBY(0,750)");
+        }
+
+
+        //b. 750 pixels up 10 times.
+
+        for (int i = 0; i < 10 ; i++) {
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBY(0,-750)");
+        }
+
+        //JavaScript method to use : window.scrollBY(0,0)
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
