25a0e4926c712e5359c6e985972163efa2aad837
Rabia Tabassum <rabiatabbasam@yahoo.com>
2023-01-21 12:00:34 +0000

    day 11: Actions

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T4_Scroll_Practice.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T4_Scroll_Practice.java
@@ -11,30 +11,56 @@ import org.testng.annotations.Test;
 public class T4_Scroll_Practice {
 
     @Test
-    public void scroll_test(){
+    public void scroll_task4_5_test(){
         //1- Open a Chrome Browser
         //2- Go to: https://practice.cydeo.com/
         Driver.getDriver().get("https://practice.cydeo.com/");
 
      //Locate the element
-        WebElement poweredByCydeo = Driver.getDriver().findElement(By.xpath("//div[contains(text(),\"Powered by\")]"));
+       // WebElement poweredByCydeo = Driver.getDriver().findElement(By.xpath("//div[contains(text(),\"Powered by\")]"));
 
         //3- Scroll down to "Powered by Cydeo"
-        //4- Scroll using Actions class "moveTo(element)"method
+        //Create object of Actions and pass our "driver" instance
         Actions actions = new Actions(Driver.getDriver());
 
+        WebElement cydeoLink = Driver.getDriver().findElement(By.linkText("CYDEO"));
+        //4- Scroll using Actions class "moveTo(element)"method
+
         BrowserUtils.sleep(2);
-        actions.moveToElement(poweredByCydeo).perform();
+        actions.moveToElement(cydeoLink).perform();
 
         //Assert.assertTrue(poweredByCydeo.isDisplayed());
         //TC#5: Scroll practice 2
 
         BrowserUtils.sleep(1);
         //2.Scroll backup to "Home" link using PageUP button
-        actions.sendKeys(Keys.PAGE_UP).perform();
+        actions.sendKeys(Keys.PAGE_UP,Keys.PAGE_UP).perform();
 
+        Driver.closeDriver();
 
+    }
+@Test
+    public void test2(){
+        Driver.getDriver().get("https://practice.cydeo.com/");
+        Driver.closeDriver();
+    }
+
+    @Test
+    public void test3(){
+        Driver.getDriver().get("https://practice.cydeo.com/");
+        Driver.closeDriver();
+    }
+
+    @Test
+    public void test4(){
+        Driver.getDriver().get("https://practice.cydeo.com/");
+        Driver.closeDriver();
     }
 
 
+
+
+
 }
+
+
--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -63,7 +63,16 @@ public class Driver {
         }
 
         return driver;
-
+    }
+    /*
+    This method will make sure our driver value is always null after using quit() method
+     */
+    //try to close the WebDriver
+    public static void closeDriver(){
+     if(driver!= null){
+         driver.quit();//this line will terminate the existing session. value will not even be null
+     driver = null;
+     }
     }
 
 }
