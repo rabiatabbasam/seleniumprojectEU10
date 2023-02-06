9b4dfe6f35cccef7cb46c3b0ea696ac0cd6fef86
Rabia Tabassum <rabiatabbasam@yahoo.com>
2023-02-06 12:00:37 +0000

    day 12: POMPractices.java.java

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
@@ -3,20 +3,30 @@ package com.cydeo.tests.day12_pom_design_explicit_wait;
 import com.cydeo.pages.LibraryLoginPage;
 import com.cydeo.utilities.Driver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class POMPractices {
 LibraryLoginPage libraryLoginPage;
-    @Test
+
+
+@BeforeMethod
+public void setupMethod(){
+    Driver.getDriver().get("https://library1.cydeo.com/");
+
+    libraryLoginPage = new LibraryLoginPage();
+}
+
+
+
+
+@Test
     public void required_field_error_message_test(){
      //TC #1: Required field error message test
      //1- Open a Chrome browser
      //2- Go to: https://library1.cydeo.com/
-        Driver.getDriver().get("https://library1.cydeo.com/");
-
-        //3- Do not enter any information
-        //4- Click the "Sign in" button
-         libraryLoginPage = new LibraryLoginPage();
+    //3- Do not enter any information
+    //4- Click the "Sign in" button
 
         libraryLoginPage.signInButton.click();
         //5- Verify expected error is displayed:
@@ -33,11 +43,7 @@ LibraryLoginPage libraryLoginPage;
          //TC #1: Required field error message test
          //1- Open a Chrome browser
          //2- Go to: https://library1.cydeo.com/
-         Driver.getDriver().get("https://library1.cydeo.com/");
-
          //3- Enter invalid email format
-         LibraryLoginPage libraryLoginPage = new LibraryLoginPage();
-
          libraryLoginPage.inputUsername.sendKeys("somethingwrong");
 
          libraryLoginPage.signInButton.click();
@@ -56,10 +62,6 @@ LibraryLoginPage libraryLoginPage;
         //TC #3: Library negative login
         //1- Open a Chrome browser
         //2- Go to: https://library1.cydeo.com/
-        Driver.getDriver().get("https://library1.cydeo.com/");
-
-        libraryLoginPage = new LibraryLoginPage();
-
         //3- Enter incorrect username or incorrect password
 
         libraryLoginPage.inputUsername.sendKeys("wrong@username.com");
