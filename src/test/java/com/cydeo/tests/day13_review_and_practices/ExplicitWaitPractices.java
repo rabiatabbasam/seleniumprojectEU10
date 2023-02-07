284a2b2bfd2a58c870961cd3c01bc7c94ebb40bf
Rabia Tabassum <rabiatabbasam@yahoo.com>
2023-02-07 12:45:09 +0000

    day 13: T2_ExplicitWaitPractice.java

---
+++ src/test/java/com/cydeo/tests/day13_review_and_practices/ExplicitWaitPractices.java
@@ -0,0 +1,57 @@
+package com.cydeo.tests.day13_review_and_practices;
+
+import com.cydeo.pages.DynamicControlsPage;
+import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.NoSuchElementException;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ExplicitWaitPractices {
+
+    DynamicControlsPage dynamicControlsPage;
+    @BeforeMethod
+    public void setupMethod(){
+        Driver.getDriver().get("https://practice.cydeo.com/dynamic_controls");
+        dynamicControlsPage = new DynamicControlsPage();
+    }
+
+    @Test
+    public void remove_button_test(){
+        //3- Click to "Remove" button
+        dynamicControlsPage.removeButton.click();
+
+        //4- Wait until "Loading bar disappear"
+      // Driver.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        //WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
+        //wait.until(ExpectedConditions.invisibilityOf(dynamicControlsPage.loadingBar));
+        BrowserUtils.waitForInvisibilityOf(dynamicControlsPage.loadingBar);
+
+        //5- Verify:
+        //a, checkbox is not displayed
+        try {
+            Assert.assertTrue(!dynamicControlsPage.checkbox.isDisplayed());
+            //assertFalse method will pass the test if the boolean value returned is: false
+            Assert.assertFalse(dynamicControlsPage.loadingBar.isDisplayed());
+        }catch(NoSuchElementException n){
+            Assert.assertTrue(true);
+        }
+        //b."It's gone!" message is displayed.
+        Assert.assertTrue(dynamicControlsPage.message.isDisplayed());
+        Assert.assertTrue(dynamicControlsPage.message.getText().equals("it's gone!"));
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
